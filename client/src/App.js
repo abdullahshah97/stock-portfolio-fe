ab4013003fe58db6632ff99820f0aadae7dd11eb
Abdullah Shah <abdullah-shah97@hotmail.com>
2023-11-23 18:54:10 +0000

    Use the finnhub api to retrieve quote data for a symbol

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,47 @@
 import React, {useEffect, useState} from 'react';
-
+const finnhub = require('finnhub');
+//TODO: Add a form to allow users to enter a stock symbol
+//TODO: Label the data according to json databoug
 function App() {
-    
-    const [backendData, setBackendData] = useState([{}])
+
+    const [apiData, setApiData] = useState([])
+    const [symbol, setSymbol] = useState("AAPL")
+    const finnhub_api_key = finnhub.ApiClient.instance.authentications['api_key'];
+    finnhub_api_key.apiKey = "clf63cpr01qovepplvugclf63cpr01qovepplvv0" // Replace this
+    const finnhubClient = new finnhub.DefaultApi()
+
+    const fetchData = async () => {
+        finnhubClient.quote(symbol, (error, data, response) => {
+            setApiData(data)
+        });
+    }
 
     useEffect(() => {
-        fetch("/").then(
-            response => response.json()
-        ).then(
-            data => setBackendData(data)
-        )
-    }, []);
+        fetchData();
+    }, [symbol]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetchData();
+    };
 
     return (
         <div>
-            {(typeof backendData.users !== 'undefined') ? (
-                <ul>
-                    {backendData.users.map((user, index) => (
-                        <li key={index}>{user}</li>
-                    ))}
-                </ul>
-            ) : (
-                <p>Waiting for backend data...</p>
-            )}
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Symbol:
+                    <input
+                        name="symbol"
+                        value={symbol}
+                        onChange={e => setSymbol(e.target.value)}
+                    />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+            <p>
+                Symbol: {symbol}<br></br>
+                Current Price: {apiData.c}
+            </p>
         </div>
     );
 }
