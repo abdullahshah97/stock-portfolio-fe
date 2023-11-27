807e7c1879666e03dacb3e9a9f2eb602154e8858
Abdullah Shah <abdullah-shah97@hotmail.com>
2023-11-27 19:23:03 +0000

    TODO - DB Connection - not done

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import SetupDatabase from "./src/config/setup-db.js";
 import database from "./src/config/sqlConnection.js";
 
 const app = express();
@@ -8,18 +9,5 @@ app.listen(1000, () => {
 });
 
 app.get("/createDatabase", (req, res) => {
-    let databaseName = "first_db";
-    let createDatabaseQuery = `CREATE DATABASE ${databaseName}`;
-
-    database.query(createDatabaseQuery, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send("database created");
-
-        let useQuery = `USE ${databaseName}`;
-        database.query(useQuery, (err, result) => {
-            if (err) throw err;
-            console.log(result);
-        });
-    });
+    SetupDatabase();
 });
--- server/src/config/sqlConnection.js
+++ server/src/config/sqlConnection.js
@@ -3,8 +3,7 @@ import mysql from 'mysql';
 const pool = mysql.createPool({
   host: "localhost",
   user: "abdullah",
-  password: 'GrizzlyBear.001',
-  database: 'first_database'
+  password: 'GrizzlyBear.001'
 });
 
 export default pool;
