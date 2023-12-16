11802defc10820bb5bac7711da83c755c6a725f3
Abdullah Shah <abdullah-shah97@hotmail.com>
2023-12-16 00:02:14 +0000

    Added first get request to Java backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchComponents from "./components/SearchComponents";
 const finnhub = require('finnhub');
 
@@ -7,10 +7,27 @@ const App = () => {
     const finnhub_api_key = finnhub.ApiClient.instance.authentications['api_key'];
     finnhub_api_key.apiKey = REACT_APP_FINNHUB_API_KEY; // Replace this
     const finnhubClient = new finnhub.DefaultApi();
+    const[ apiData, setData ] = useState({});
 
+    useEffect(() =>
+    {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("http://localhost:8080/");
+                const json = await response.json();
+                setData(json);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchData();
+    }, [apiData]);
     return (
         <div className="App">
+
+            <p>{ apiData.message }</p>
             <SearchComponents finnhubClient= {finnhubClient}/>
+
         </div>
     );
 };
