44bd58ec2fa311fd395e92a0ca416bea88e7b460
Abdullah Shah <abdullah-shah97@hotmail.com>
2023-12-17 20:33:17 +0000

    Homepage uses Link to link to login page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
     return (
         <div>
             <h1>Welcome to the Home Page!</h1>
-            {/* Add your home page content here */}
+            <Link to="/login">Go to Login</Link>
         </div>
     );
 }
