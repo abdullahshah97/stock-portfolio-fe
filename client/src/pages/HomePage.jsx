c2109b3680eb73b9f620ff37c2f4ae1ce4f5553a
Abdullah Shah <abdullah-shah97@hotmail.com>
2023-12-20 14:59:39 +0000

    Import the header to homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from "./Header";
 
 const HomePage = () => {
     return (
         <div>
+            <Header/>
             <h1>Welcome to the Home Page!</h1>
             <Link to="/login">Go to Login</Link>
         </div>
