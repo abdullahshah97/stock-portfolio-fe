76f9e2d9f413184a815f60422e9c15d9f6abbb01
Abdullah Shah <abdullah-shah97@hotmail.com>
2024-01-08 18:59:21 +0000

    Added a link to the login page

--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -1,15 +1,22 @@
 import useSignUpDetails from '../hooks/useSignUpHook'
+import {Link} from "react-router-dom";
+import React from "react";
 
 const SignupPage = () => {
     const {signUpDetails, handleInputChange, handleSubmit } = useSignUpDetails();
 
     return (
+        <>
+            <Link to="/login">Log In</Link>
+            <h1>Sign Up Page</h1>
         <form onSubmit={handleSubmit}>
-            <input type="text" name="username" value={signUpDetails.username} onChange={e => handleInputChange(e, 'username')}/>
+            <label>Email: </label>
             <input type="email" name="email" value={signUpDetails.email} onChange={e => handleInputChange(e, 'email')}/>
+            <label>Password: </label>
             <input type="password" name="password" value={signUpDetails.password} onChange={e => handleInputChange(e, 'password')}/>
             <button type="submit">Sign Up</button>
         </form>
+        </>
     );
 };
 
